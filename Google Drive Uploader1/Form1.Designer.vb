2de5010aa8d6c93eead068725300c3b56aeefbaa
Moises Cardona <moisesmcardona@gmail.com>
2017-03-19 12:20:50 -0400

    Add project files.

---
+++ Google Drive Uploader1/Form1.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(429, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Upload"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Length: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Processed:"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 80)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(492, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(78, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Operation not started"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(78, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(106, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Operation not started"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(330, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(111, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Choose File to Upload"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(348, 23)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 294)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(98, 26)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "By Moises Cardona" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "v1.1"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(348, 64)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Status:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(394, 64)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(106, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Operation not started"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(349, 51)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(47, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Percent:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(394, 51)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(21, 13)
+        Me.Label10.TabIndex = 15
+        Me.Label10.Text = "0%"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(12, 115)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(81, 13)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "Latest Uploads:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(412, 131)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 23)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "More Results"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(15, 131)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(391, 160)
+        Me.ListBox1.TabIndex = 20
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(412, 160)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(92, 23)
+        Me.Button5.TabIndex = 21
+        Me.Button5.Text = "Download File"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(413, 268)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(91, 23)
+        Me.Button4.TabIndex = 22
+        Me.Button4.Text = "Refresh List"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(516, 330)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Google Drive Uploader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ Google Drive Uploader1/Form1.vb
@@ -0,0 +1,254 @@
+﻿Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Drive.v3
+Imports Google.Apis.Services
+Imports Google.Apis.Util.Store
+Imports System.IO
+Imports System.Threading
+Imports Google.Apis.Upload
+Imports Google.Apis.Download
+Imports System.Security.Cryptography
+
+Public Class Form1
+    Private FileIdsListBox As New ListBox
+    Private FileSizeListBox As New ListBox
+    Public pageToken As String = ""
+    ' If modifying these scopes, delete your previously saved credentials
+    ' at ~/.credentials/drive-dotnet-quickstart.json
+    Shared Scopes As String() = {DriveService.Scope.DriveFile, DriveService.Scope.Drive}
+    Shared ApplicationName As String = "Drive API .NET Quickstart"
+    Public service As DriveService
+    Private ResumeUpload As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim credential As UserCredential
+
+        Using stream = New FileStream("client_secret.json", FileMode.Open, FileAccess.Read)
+            Dim credPath As String = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal)
+            credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json")
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None, New FileDataStore(credPath, True)).Result
+            Console.WriteLine(Convert.ToString("Credential file saved to: ") & credPath)
+        End Using
+        ' Create Drive API service.
+        Dim Initializer As New BaseClientService.Initializer()
+        Initializer.HttpClientInitializer = credential
+        Initializer.ApplicationName = ApplicationName
+        service = New DriveService(Initializer)
+
+        ' Define parameters of request.
+        Dim listRequest As FilesResource.ListRequest = service.Files.List()
+        listRequest.PageSize = 10
+        listRequest.Fields = "nextPageToken, files(id, name, size)"
+
+        ' List files.
+        Dim files = listRequest.Execute()
+        If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
+            For Each file In files.Files
+                ListBox1.Items.Add(file.Name)
+                FileIdsListBox.Items.Add(file.Id)
+                Try
+                    FileSizeListBox.Items.Add(file.Size)
+                Catch
+                    FileSizeListBox.Items.Add("0")
+                End Try
+            Next
+        End If
+        pageToken = files.NextPageToken
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim listRequest As FilesResource.ListRequest = service.Files.List()
+        listRequest.PageSize = 10
+        listRequest.Fields = "nextPageToken, files(id, name, size)"
+        listRequest.PageToken = pageToken
+        ' List files.
+        Dim files = listRequest.Execute()
+        If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
+            For Each file In files.Files
+                ListBox1.Items.Add(file.Name)
+                FileIdsListBox.Items.Add(file.Id)
+                Try
+                    FileSizeListBox.Items.Add(file.Size)
+                Catch
+                    FileSizeListBox.Items.Add("0")
+                End Try
+            Next
+        End If
+        pageToken = files.NextPageToken
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label3.Text = String.Format("{0:F2} MB", My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length / 1024 / 1024)
+        ProgressBar1.Maximum = My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length / 1024 / 1024
+        Dim FileMetadata As New Data.File
+        FileMetadata.Name = My.Computer.FileSystem.GetName(TextBox1.Text)
+        Dim UploadStream As New FileStream(TextBox1.Text, System.IO.FileMode.Open, System.IO.FileAccess.Read)
+        Dim UploadFile As FilesResource.CreateMediaUpload = service.Files.Create(FileMetadata, UploadStream, "")
+        AddHandler UploadFile.ProgressChanged, New Action(Of IUploadProgress)(AddressOf Upload_ProgressChanged)
+        AddHandler UploadFile.ResponseReceived, New Action(Of Data.File)(AddressOf Upload_ResponseReceived)
+        AddHandler UploadFile.UploadSessionData, AddressOf Upload_UploadSessionData
+        UploadCancellationToken = New System.Threading.CancellationToken
+        Dim uploadUri As Uri = GetSessionRestartUri()
+        If uploadUri = Nothing Then
+            UploadFile.UploadAsync(UploadCancellationToken)
+        Else
+            Console.WriteLine("Restarting prior upload session.")
+            UploadFile.ResumeAsync(uploadUri, UploadCancellationToken)
+        End If
+
+    End Sub
+    Private ErrorMessage As String = ""
+    Private UploadCancellationToken As System.Threading.CancellationToken
+    Shared BytesSentText As Long
+    Shared UploadStatusText As String
+    Private Sub Upload_ProgressChanged(uploadStatusInfo As IUploadProgress)
+        Select Case uploadStatusInfo.Status
+            Case UploadStatus.Completed
+                UploadStatusText = "Completed!!"
+                BytesSentText = My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length
+                UpdateBytesSent()
+            Case UploadStatus.Starting
+                BytesSentText = "0"
+                UploadStatusText = "Starting..."
+                UpdateBytesSent()
+            Case UploadStatus.Uploading
+                BytesSentText = uploadStatusInfo.BytesSent
+                UploadStatusText = "Uploading..."
+                UpdateBytesSent()
+            Case UploadStatus.Failed
+                Dim APIException As Google.GoogleApiException = TryCast(uploadStatusInfo.Exception, Google.GoogleApiException)
+                If (APIException Is Nothing) OrElse (APIException.Error Is Nothing) Then
+                    MsgBox(uploadStatusInfo.Exception.Message)
+                Else
+                    MsgBox(APIException.Error.ToString())
+                    ' Do not retry if the request is in error
+                    Dim StatusCode As Int32 = CInt(APIException.HttpStatusCode)
+                    ' See https://developers.google.com/youtube/v3/guides/using_resumable_upload_protocol
+                    If ((StatusCode / 100) = 4 OrElse ((StatusCode / 100) = 5 AndAlso Not (StatusCode = 500 Or StatusCode = 502 Or StatusCode = 503 Or StatusCode = 504))) Then
+                        MsgBox("Cannot retry upload...")
+                    End If
+                End If
+                MsgBox("Failed...")
+        End Select
+    End Sub
+    Private Sub Upload_ResponseReceived(file As Data.File)
+        MsgBox("File Upload Completed!")
+    End Sub
+    Private Sub Upload_UploadSessionData(ByVal uploadSessionData As IUploadSessionData)
+        ' Save UploadUri.AbsoluteUri and FullPath Filename values for use if program faults and we want to restart the program
+        My.Settings.ResumeUri = uploadSessionData.UploadUri.AbsoluteUri
+        My.Settings.ResumeFilename = TextBox1.Text
+        ' Saved to a user.config file within a subdirectory of C:\Users\<yourusername>\AppData\Local
+        My.Settings.Save()
+
+    End Sub
+    Private Function GetSessionRestartUri() As Uri
+        If My.Settings.ResumeUri.Length > 0 AndAlso My.Settings.ResumeFilename = TextBox1.Text Then
+            ' An UploadUri from a previous execution is present, ask if a resume should be attempted
+            If MsgBox(String.Format("Resume previous upload?{0}{0}{1}", vbNewLine, TextBox1.Text), MsgBoxStyle.Question Or MsgBoxStyle.YesNo, "Resume Upload") = MsgBoxResult.Yes Then
+                Return New Uri(My.Settings.ResumeUri)
+            Else
+                Return Nothing
+            End If
+        Else
+            Return Nothing
+        End If
+    End Function
+    Private Sub UpdateBytesSent()
+        If Label4.InvokeRequired Then
+            Dim method As MethodInvoker = New MethodInvoker(AddressOf UpdateBytesSent)
+            Invoke(method)
+        End If
+        If Label8.InvokeRequired Then
+            Dim method As MethodInvoker = New MethodInvoker(AddressOf UpdateBytesSent)
+            Invoke(method)
+        End If
+        If ProgressBar1.InvokeRequired Then
+            Dim method As MethodInvoker = New MethodInvoker(AddressOf UpdateBytesSent)
+            Invoke(method)
+        End If
+        Label4.Text = String.Format("{0:F2} MB", BytesSentText / 1024 / 1024)
+        Label8.Text = UploadStatusText
+        Try
+            ProgressBar1.Value = BytesSentText / 1024 / 1024
+        Catch
+
+        End Try
+        Label10.Text = String.Format("{0:F2}%", ((ProgressBar1.Value / ProgressBar1.Maximum) * 100))
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        OpenFileDialog1.Title = "Select a file to Upload"
+        OpenFileDialog1.FileName = My.Computer.FileSystem.GetName(TextBox1.Text)
+        OpenFileDialog1.ShowDialog()
+        TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+    Private Shared FileToSave As FileStream
+    Private Shared MaxFileSize
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        FileIdsListBox.SelectedIndex = ListBox1.SelectedIndex
+        FileSizeListBox.SelectedIndex = ListBox1.SelectedIndex
+        SaveFileDialog1.Title = "Browse for a location to save the file:"
+        SaveFileDialog1.FileName = ListBox1.SelectedItem
+        Dim SFDResult As MsgBoxResult = SaveFileDialog1.ShowDialog()
+        If SFDResult = MsgBoxResult.Ok Then
+            Label3.Text = String.Format("{0:F2} MB", FileSizeListBox.SelectedItem / 1024 / 1024)
+            ProgressBar1.Maximum = FileSizeListBox.SelectedItem / 1024 / 1024
+            MaxFileSize = FileSizeListBox.SelectedItem
+            FileToSave = New FileStream(SaveFileDialog1.FileName, FileMode.Create, FileAccess.Write)
+            Dim DownloadRequest As FilesResource.GetRequest = service.Files.Get(FileIdsListBox.SelectedItem.ToString)
+            AddHandler DownloadRequest.MediaDownloader.ProgressChanged, New Action(Of IDownloadProgress)(AddressOf Download_ProgressChanged)
+            DownloadRequest.DownloadAsync(FileToSave)
+        End If
+    End Sub
+    Private Sub Download_ProgressChanged(progress As IDownloadProgress)
+        Select Case progress.Status
+            Case DownloadStatus.Completed
+                UploadStatusText = "Completed!!"
+                FileToSave.Close()
+                BytesSentText = MaxFileSize
+                UpdateBytesSent()
+
+            Case DownloadStatus.Downloading
+                BytesSentText = progress.BytesDownloaded
+                UploadStatusText = "Downloading..."
+                UpdateBytesSent()
+            Case UploadStatus.Failed
+                UploadStatusText = "Failed..."
+                UpdateBytesSent()
+        End Select
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+        FileIdsListBox.Items.Clear()
+        FileSizeListBox.Items.Clear()
+        Dim listRequest As FilesResource.ListRequest = service.Files.List()
+        listRequest.PageSize = 10
+        listRequest.Fields = "nextPageToken, files(id, name, size)"
+        listRequest.PageToken = ""
+        ' List files.
+        Dim files = listRequest.Execute()
+        If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
+            For Each file In files.Files
+                ListBox1.Items.Add(file.Name)
+                FileIdsListBox.Items.Add(file.Id)
+                Try
+                    FileSizeListBox.Items.Add(file.Size)
+                Catch
+                    FileSizeListBox.Items.Add("0")
+                End Try
+            Next
+        End If
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        Dim filepath() As String = e.Data.GetData(DataFormats.FileDrop)
+        For Each path In filepath
+            TextBox1.Text = path
+        Next
+    End Sub
+    Private Sub Form1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+End Class
