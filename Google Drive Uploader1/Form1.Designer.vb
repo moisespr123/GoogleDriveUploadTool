85559d2c8d47991a51a45e589e43a99c4e97adc8
Moises Cardona <moisesmcardona@gmail.com>
2017-03-20 08:59:30 -0400

    v1.4 Batch Upload added!

--- Google Drive Uploader1/Form1.Designer.vb
+++ Google Drive Uploader1/Form1.Designer.vb
@@ -28,9 +28,7 @@ Partial Class Form1
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
@@ -50,14 +48,16 @@ Partial Class Form1
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button6 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(429, 23)
+        Me.Button2.Location = New System.Drawing.Point(351, 235)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(153, 39)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Upload"
         Me.Button2.UseVisualStyleBackColor = True
@@ -65,7 +65,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 98)
+        Me.Label1.Location = New System.Drawing.Point(12, 277)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(46, 13)
         Me.Label1.TabIndex = 3
@@ -74,7 +74,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 111)
+        Me.Label2.Location = New System.Drawing.Point(12, 290)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(60, 13)
         Me.Label2.TabIndex = 4
@@ -82,7 +82,7 @@ Partial Class Form1
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(12, 127)
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 306)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(492, 23)
         Me.ProgressBar1.TabIndex = 5
@@ -90,7 +90,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(78, 98)
+        Me.Label3.Location = New System.Drawing.Point(78, 277)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(27, 13)
         Me.Label3.TabIndex = 6
@@ -99,36 +99,20 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(78, 111)
+        Me.Label4.Location = New System.Drawing.Point(78, 290)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(27, 13)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "N/A"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(330, 20)
-        Me.TextBox1.TabIndex = 8
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Location = New System.Drawing.Point(12, 9)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(111, 13)
+        Me.Label5.Size = New System.Drawing.Size(205, 13)
         Me.Label5.TabIndex = 9
-        Me.Label5.Text = "Choose File to Upload"
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(348, 23)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 10
-        Me.Button3.Text = "Browse"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Label5.Text = "Drag and Drop Files to add them to the list"
         '
         'OpenFileDialog1
         '
@@ -141,12 +125,12 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(98, 26)
         Me.Label6.TabIndex = 11
-        Me.Label6.Text = "By Moises Cardona" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "v1.3.1"
+        Me.Label6.Text = "By Moises Cardona" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "v1.4"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(348, 111)
+        Me.Label7.Location = New System.Drawing.Point(348, 290)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(40, 13)
         Me.Label7.TabIndex = 12
@@ -155,7 +139,7 @@ Partial Class Form1
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(402, 111)
+        Me.Label8.Location = New System.Drawing.Point(402, 290)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(27, 13)
         Me.Label8.TabIndex = 13
@@ -164,7 +148,7 @@ Partial Class Form1
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(349, 98)
+        Me.Label9.Location = New System.Drawing.Point(349, 277)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(44, 13)
         Me.Label9.TabIndex = 14
@@ -173,7 +157,7 @@ Partial Class Form1
         'Label10
         '
         Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(402, 98)
+        Me.Label10.Location = New System.Drawing.Point(402, 277)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(21, 13)
         Me.Label10.TabIndex = 15
@@ -182,7 +166,7 @@ Partial Class Form1
         'Label11
         '
         Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(9, 153)
+        Me.Label11.Location = New System.Drawing.Point(534, 9)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(139, 13)
         Me.Label11.TabIndex = 17
@@ -190,7 +174,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(409, 169)
+        Me.Button1.Location = New System.Drawing.Point(934, 25)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(92, 23)
         Me.Button1.TabIndex = 18
@@ -200,14 +184,14 @@ Partial Class Form1
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(12, 169)
+        Me.ListBox1.Location = New System.Drawing.Point(537, 25)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(391, 160)
+        Me.ListBox1.Size = New System.Drawing.Size(391, 342)
         Me.ListBox1.TabIndex = 20
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(409, 198)
+        Me.Button5.Location = New System.Drawing.Point(934, 54)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(92, 23)
         Me.Button5.TabIndex = 21
@@ -216,7 +200,7 @@ Partial Class Form1
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(410, 306)
+        Me.Button4.Location = New System.Drawing.Point(935, 343)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(91, 23)
         Me.Button4.TabIndex = 22
@@ -226,7 +210,7 @@ Partial Class Form1
         'Label12
         '
         Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(12, 48)
+        Me.Label12.Location = New System.Drawing.Point(12, 235)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(264, 13)
         Me.Label12.TabIndex = 23
@@ -234,7 +218,7 @@ Partial Class Form1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 64)
+        Me.TextBox2.Location = New System.Drawing.Point(12, 251)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(330, 20)
         Me.TextBox2.TabIndex = 24
@@ -243,7 +227,7 @@ Partial Class Form1
         'Label13
         '
         Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(208, 111)
+        Me.Label13.Location = New System.Drawing.Point(208, 290)
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(57, 13)
         Me.Label13.TabIndex = 25
@@ -252,7 +236,7 @@ Partial Class Form1
         'Label14
         '
         Me.Label14.AutoSize = True
-        Me.Label14.Location = New System.Drawing.Point(271, 111)
+        Me.Label14.Location = New System.Drawing.Point(271, 290)
         Me.Label14.Name = "Label14"
         Me.Label14.Size = New System.Drawing.Size(49, 13)
         Me.Label14.TabIndex = 26
@@ -289,12 +273,31 @@ Partial Class Form1
         Me.RadioButton1.Text = "English"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(12, 25)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(492, 173)
+        Me.ListBox2.TabIndex = 28
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 204)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(205, 23)
+        Me.Button6.TabIndex = 29
+        Me.Button6.Text = "Remove selected file from list"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(516, 384)
+        Me.ClientSize = New System.Drawing.Size(1036, 384)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.ListBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label14)
         Me.Controls.Add(Me.Label13)
@@ -310,9 +313,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.ProgressBar1)
@@ -333,9 +334,7 @@ Partial Class Form1
     Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label5 As Label
-    Friend WithEvents Button3 As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
@@ -355,4 +354,6 @@ Partial Class Form1
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button6 As Button
 End Class
--- Google Drive Uploader1/Form1.vb
+++ Google Drive Uploader1/Form1.vb
@@ -45,78 +45,77 @@ Public Class Form1
         Initializer.ApplicationName = ApplicationName
         service = New DriveService(Initializer)
         service.HttpClient.Timeout = TimeSpan.FromSeconds(240)
-        ' Define parameters of request.
-        Dim listRequest As FilesResource.ListRequest = service.Files.List()
-        listRequest.PageSize = 10
-        listRequest.Fields = "nextPageToken, files(id, name, size)"
-
         ' List files.
-        Dim files = listRequest.Execute()
-        If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
-            For Each file In files.Files
-                ListBox1.Items.Add(file.Name)
-                FileIdsListBox.Items.Add(file.Id)
-                Try
-                    FileSizeListBox.Items.Add(file.Size)
-                Catch
-                    FileSizeListBox.Items.Add("0")
-                End Try
-            Next
-        End If
-        pageToken = files.NextPageToken
+        ShowList()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ShowList()
+    End Sub
+    Private Sub ShowList()
         Dim listRequest As FilesResource.ListRequest = service.Files.List()
-        listRequest.PageSize = 10
+        listRequest.PageSize = 25
         listRequest.Fields = "nextPageToken, files(id, name, size)"
         listRequest.PageToken = pageToken
         ' List files.
-        Dim files = listRequest.Execute()
-        If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
-            For Each file In files.Files
-                ListBox1.Items.Add(file.Name)
-                FileIdsListBox.Items.Add(file.Id)
-                Try
-                    FileSizeListBox.Items.Add(file.Size)
-                Catch
-                    FileSizeListBox.Items.Add("0")
-                End Try
-            Next
-        End If
-        pageToken = files.NextPageToken
+        Try
+            Dim files = listRequest.Execute()
+            If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
+                For Each file In files.Files
+                    ListBox1.Items.Add(file.Name)
+                    FileIdsListBox.Items.Add(file.Id)
+                    Try
+                        FileSizeListBox.Items.Add(file.Size)
+                    Catch
+                        FileSizeListBox.Items.Add("0")
+                    End Try
+                Next
+            End If
+            pageToken = files.NextPageToken
+        Catch ex As Exception
+        End Try
     End Sub
     Private starttime As DateTime
     Private timespent As TimeSpan
     Private secondsremaining As Integer = 0
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Label3.Text = String.Format("{0:F2} MB", My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length / 1024 / 1024)
-        ProgressBar1.Maximum = My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length / 1024 / 1024
-        Dim FileMetadata As New Data.File
-        FileMetadata.Name = My.Computer.FileSystem.GetName(TextBox1.Text)
-        Dim FileFolder As New List(Of String)
-        If String.IsNullOrEmpty(TextBox2.Text) = False Then
-            FileFolder.Add(TextBox2.Text)
-        Else
-            FileFolder.Add("root")
-        End If
-        FileMetadata.Parents = FileFolder
-        Dim UploadStream As New FileStream(TextBox1.Text, System.IO.FileMode.Open, System.IO.FileAccess.Read)
-        FileMetadata.ModifiedTime = IO.File.GetLastWriteTimeUtc(TextBox1.Text)
-        Dim UploadFile As FilesResource.CreateMediaUpload = service.Files.Create(FileMetadata, UploadStream, "")
-        UploadFile.ChunkSize = ResumableUpload.MinimumChunkSize * 4
-        AddHandler UploadFile.ProgressChanged, New Action(Of IUploadProgress)(AddressOf Upload_ProgressChanged)
-        AddHandler UploadFile.ResponseReceived, New Action(Of Data.File)(AddressOf Upload_ResponseReceived)
-        AddHandler UploadFile.UploadSessionData, AddressOf Upload_UploadSessionData
-        UploadCancellationToken = New CancellationToken
-        Dim uploadUri As Uri = GetSessionRestartUri()
-        starttime = DateTime.Now
-        If uploadUri = Nothing Then
-            UploadFile.UploadAsync(UploadCancellationToken)
-        Else
-            UploadFile.ResumeAsync(uploadUri, UploadCancellationToken)
-        End If
-
+    Private GetFile As String = ""
+    Private UploadFailed As Boolean = False
+    Private Async Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim NumberOfFilesToUpload As Integer = ListBox2.Items.Count
+        For i As Integer = 0 To NumberOfFilesToUpload - 1
+            UploadFailed = False
+            GetFile = ListBox2.Items.Item(0)
+            Label3.Text = String.Format("{0:F2} MB", My.Computer.FileSystem.GetFileInfo(GetFile).Length / 1024 / 1024)
+            ProgressBar1.Maximum = My.Computer.FileSystem.GetFileInfo(GetFile).Length / 1024 / 1024
+            Dim FileMetadata As New Data.File
+            FileMetadata.Name = My.Computer.FileSystem.GetName(GetFile)
+            Dim FileFolder As New List(Of String)
+            If String.IsNullOrEmpty(TextBox2.Text) = False Then
+                FileFolder.Add(TextBox2.Text)
+            Else
+                FileFolder.Add("root")
+            End If
+            FileMetadata.Parents = FileFolder
+            Dim UploadStream As New FileStream(GetFile, System.IO.FileMode.Open, System.IO.FileAccess.Read)
+            FileMetadata.ModifiedTime = IO.File.GetLastWriteTimeUtc(GetFile)
+            Dim UploadFile As FilesResource.CreateMediaUpload = service.Files.Create(FileMetadata, UploadStream, "")
+            UploadFile.ChunkSize = ResumableUpload.MinimumChunkSize * 4
+            AddHandler UploadFile.ProgressChanged, New Action(Of IUploadProgress)(AddressOf Upload_ProgressChanged)
+            AddHandler UploadFile.ResponseReceived, New Action(Of Data.File)(AddressOf Upload_ResponseReceived)
+            AddHandler UploadFile.UploadSessionData, AddressOf Upload_UploadSessionData
+            UploadCancellationToken = New CancellationToken
+            Dim uploadUri As Uri = GetSessionRestartUri()
+            starttime = DateTime.Now
+            If uploadUri = Nothing Then
+                Await UploadFile.UploadAsync(UploadCancellationToken)
+            Else
+                Await UploadFile.ResumeAsync(uploadUri, UploadCancellationToken)
+            End If
+            If UploadFailed = False Then
+                ListBox2.Items.RemoveAt(0)
+                RefreshFileList()
+            End If
+        Next
     End Sub
     Private ErrorMessage As String = ""
     Private UploadCancellationToken As System.Threading.CancellationToken
@@ -126,7 +125,7 @@ Public Class Form1
         Select Case uploadStatusInfo.Status
             Case UploadStatus.Completed
                 If RadioButton1.Checked = True Then UploadStatusText = "Completed!!" Else UploadStatusText = "¡Completado!"
-                BytesSentText = My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length
+                BytesSentText = My.Computer.FileSystem.GetFileInfo(GetFile).Length
                 UpdateBytesSent()
             Case UploadStatus.Starting
                 BytesSentText = "0"
@@ -156,25 +155,26 @@ Public Class Form1
                     End If
                 End If
                 If RadioButton1.Checked = True Then UploadStatusText = "Failed..." Else UploadStatusText = "Error..."
+                UploadFailed = True
                 UpdateBytesSent()
         End Select
     End Sub
     Private Sub Upload_ResponseReceived(file As Data.File)
         If RadioButton1.Checked = True Then UploadStatusText = "Completed!!" Else UploadStatusText = "¡Completado!"
-        BytesSentText = My.Computer.FileSystem.GetFileInfo(TextBox1.Text).Length
+        BytesSentText = My.Computer.FileSystem.GetFileInfo(GetFile).Length
         UpdateBytesSent()
-        RefreshFileList()
+
     End Sub
     Private Sub Upload_UploadSessionData(ByVal uploadSessionData As IUploadSessionData)
         ' Save UploadUri.AbsoluteUri and FullPath Filename values for use if program faults and we want to restart the program
         My.Settings.ResumeUri = uploadSessionData.UploadUri.AbsoluteUri
-        My.Settings.ResumeFilename = TextBox1.Text
+        My.Settings.ResumeFilename = GetFile
         ' Saved to a user.config file within a subdirectory of C:\Users\<yourusername>\AppData\Local
         My.Settings.Save()
 
     End Sub
     Private Function GetSessionRestartUri() As Uri
-        If My.Settings.ResumeUri.Length > 0 AndAlso My.Settings.ResumeFilename = TextBox1.Text Then
+        If My.Settings.ResumeUri.Length > 0 AndAlso My.Settings.ResumeFilename = GetFile Then
             ' An UploadUri from a previous execution is present, ask if a resume should be attempted
             Dim ResumeText1 As String = ""
             Dim ResumeText2 As String = ""
@@ -187,7 +187,7 @@ Public Class Form1
                 ResumeText2 = "Resumir"
             End If
 
-            If MsgBox(String.Format(ResumeText1, vbNewLine, TextBox1.Text), MsgBoxStyle.Question Or MsgBoxStyle.YesNo, ResumeText2) = MsgBoxResult.Yes Then
+            If MsgBox(String.Format(ResumeText1, vbNewLine, GetFile), MsgBoxStyle.Question Or MsgBoxStyle.YesNo, ResumeText2) = MsgBoxResult.Yes Then
                 Return New Uri(My.Settings.ResumeUri)
             Else
                 Return Nothing
@@ -223,17 +223,6 @@ Public Class Form1
         Label10.Text = String.Format("{0:F2}%", ((ProgressBar1.Value / ProgressBar1.Maximum) * 100))
         Dim timeFormatted As TimeSpan = TimeSpan.FromSeconds(secondsremaining)
         Label14.Text = String.Format("{0}:{1:mm}:{1:ss}", CInt(Math.Truncate(timeFormatted.TotalHours)), timeFormatted)
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        If RadioButton1.Checked = True Then
-            OpenFileDialog1.Title = "Select a file to Upload"
-        Else
-            OpenFileDialog1.Title = "Seleccione un archivo para subir"
-        End If
-        OpenFileDialog1.FileName = My.Computer.FileSystem.GetName(TextBox1.Text)
-        OpenFileDialog1.ShowDialog()
-        TextBox1.Text = OpenFileDialog1.FileName
     End Sub
     Private Shared FileToSave As FileStream
     Private Shared MaxFileSize
@@ -281,31 +270,38 @@ Public Class Form1
     End Sub
 
     Private Sub RefreshFileList()
+        If ListBox1.InvokeRequired Then
+            Dim method As MethodInvoker = New MethodInvoker(AddressOf RefreshFileList)
+            Invoke(method)
+        End If
         ListBox1.Items.Clear()
         FileIdsListBox.Items.Clear()
         FileSizeListBox.Items.Clear()
         Dim listRequest As FilesResource.ListRequest = service.Files.List()
-        listRequest.PageSize = 10
+        listRequest.PageSize = 25
         listRequest.Fields = "nextPageToken, files(id, name, size)"
         listRequest.PageToken = ""
         ' List files.
-        Dim files = listRequest.Execute()
-        If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
-            For Each file In files.Files
-                ListBox1.Items.Add(file.Name)
-                FileIdsListBox.Items.Add(file.Id)
-                Try
-                    FileSizeListBox.Items.Add(file.Size)
-                Catch
-                    FileSizeListBox.Items.Add("0")
-                End Try
-            Next
-        End If
+        Try
+            Dim files = listRequest.Execute()
+            If files.Files IsNot Nothing AndAlso files.Files.Count > 0 Then
+                For Each file In files.Files
+                    ListBox1.Items.Add(file.Name)
+                    FileIdsListBox.Items.Add(file.Id)
+                    Try
+                        FileSizeListBox.Items.Add(file.Size)
+                    Catch
+                        FileSizeListBox.Items.Add("0")
+                    End Try
+                Next
+            End If
+        Catch ex As Exception
+        End Try
     End Sub
     Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
         Dim filepath() As String = e.Data.GetData(DataFormats.FileDrop)
         For Each path In filepath
-            TextBox1.Text = path
+            ListBox2.Items.Add(path)
         Next
     End Sub
     Private Sub Form1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
@@ -328,8 +324,8 @@ Public Class Form1
     Private Sub EnglishLanguage()
         Label1.Text = "Length:"
         Label2.Text = "Processed:"
-        Label5.Text = "Choose File to Upload"
-        Label6.Text = "By Moises Cardona" & vbNewLine & "v1.3.1"
+        Label5.Text = "Drag and Drop Files to add them to the list"
+        Label6.Text = "By Moises Cardona" & vbNewLine & "v1.4"
         Label7.Text = "Status:"
         Label9.Text = "Percent: "
         Label11.Text = "Uploads (By Date Modified):"
@@ -337,16 +333,15 @@ Public Class Form1
         Label13.Text = "Time Left: "
         Button1.Text = "More Results"
         Button2.Text = "Upload"
-        Button3.Text = "Browse"
         Button4.Text = "Refresh List"
         Button5.Text = "Download File"
-
+        Button6.Text = "Remove selected file from list"
     End Sub
     Private Sub SpanishLanguage()
         Label1.Text = "Tamaño:"
         Label2.Text = "Procesado:"
-        Label5.Text = "Seleccione un archivo para subir"
-        Label6.Text = "Por Moises Cardona" & vbNewLine & "v1.3.1"
+        Label5.Text = "Arrastre archivos aquí para añadirlos a la lista"
+        Label6.Text = "Por Moises Cardona" & vbNewLine & "v1.4"
         Label7.Text = "Estado:"
         Label9.Text = "Porcentaje: "
         Label11.Text = "Archivos subidos (Organizados por fecha de modificación):"
@@ -354,9 +349,12 @@ Public Class Form1
         Label13.Text = "Tiempo Est."
         Button1.Text = "Más Resultados"
         Button2.Text = "Subir"
-        Button3.Text = "Buscar"
         Button4.Text = "Refrescar Lista"
         Button5.Text = "Descargar Archivo"
+        Button6.Text = "Remover archivo de la lista"
     End Sub
 
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ListBox2.Items.RemoveAt(ListBox2.SelectedIndex)
+    End Sub
 End Class
