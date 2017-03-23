b8c8f9abd3520e3d5dda5260f84ef6604a1694a3
Moises Cardona <cardona_76937@students.pupr.edu>
2017-03-23 10:29:10 -0400

    Fixed Preserve File Modified Date location

--- Google Drive Uploader1/Form1.Designer.vb
+++ Google Drive Uploader1/Form1.Designer.vb
@@ -303,7 +303,7 @@ Partial Class Form1
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(348, 204)
+        Me.CheckBox1.Location = New System.Drawing.Point(272, 204)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(156, 17)
         Me.CheckBox1.TabIndex = 30
